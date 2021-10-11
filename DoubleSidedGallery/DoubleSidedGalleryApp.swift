373a75b3bbe3fe227852110d0fdd14d71512c5e7
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-11 19:58:54 +0700

    Create the project

---
+++ DoubleSidedGallery/DoubleSidedGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleSidedGalleryApp.swift
+//  DoubleSidedGallery
+//
+//  Created by Danh Tu on 11/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct DoubleSidedGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
